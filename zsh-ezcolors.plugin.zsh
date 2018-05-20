12e688a90407eabecce9073e18435330cd1e1fd2
Matt Stofko <matt@mjslabs.com>
2018-05-20 15:08:20 -0700

    Add export.

--- zsh-ezcolors.plugin.zsh
+++ zsh-ezcolors.plugin.zsh
@@ -4,7 +4,7 @@ if [[ "$terminfo[colors]" -gt 8 ]]; then
     colors
 fi
 for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
-    eval $COLOR='$fg_no_bold[${(L)COLOR}]'
-    eval BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
+    eval export $COLOR='$fg_no_bold[${(L)COLOR}]'
+    eval export BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
 done
-eval RESET='$reset_color'
+eval export RESET='$reset_color'
